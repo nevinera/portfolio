2af3460b41d5fdcbdfad789978891048d22eb920
Eric Mueller <emueller@emcien.com>
2011-05-17 09:51:27 -0400

    initial rails project + haml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '9d3032ccd8422cd6f48099a339cb7ff0ba430a3075b80c7a40f264b6d540e69e11849e88bc85dace5841e130b2e86be2bb98336ca3b7ac031fddbe22d84790fb'
