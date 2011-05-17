c937be01018ac58d133f8ea9312adf11617703c1
Eric Mueller <emueller@emcien.com>
2011-05-17 18:15:55 -0400

    removed refresh endpoint - either wait till cron job refreshes it, or do it from command line. For now.

--- app/controllers/owners_controller.rb
+++
@@ -1,9 +0,0 @@
-class OwnersController < ApplicationController
-  def refresh
-    @own = Owner.find_by_id(params[:id])
-    raise "Owner #{params[:id]} not found" unless @own.present?
-
-    @own.rebuild_galleries_from_imgur!
-    redirect_to "/"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Portfolio::Application.routes.draw do
   match "/galleries/:shortname" => "galleries#show"
   match "/galleries" => "galleries#index"
-  match "/owners/:id/refresh" => "owners#refresh"
 
   root :to => "galleries#splash"
 end
